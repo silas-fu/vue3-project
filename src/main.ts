44c2f5931d6c022bae0e00aa7cc94b712e2456dc
silas fu <rexfu0126@gmail.com>
2025-12-28 12:36:39 +0800

    modify css file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
