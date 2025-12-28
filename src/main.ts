20fe06342879368b72f04bd3dfdddd1715f95abf
silas fu <rexfu0126@gmail.com>
2025-12-28 11:48:20 +0800

    inject bs5

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import App from './App.vue'
 import router from './router'
