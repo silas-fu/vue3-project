29c039edcdcc2fb5f3afde14836c292e44843a87
silas fu <rexfu0126@gmail.com>
2025-12-28 13:04:21 +0800

    setting mixin and reset css

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,9 @@
+<template>
+  <RouterView />
+</template>
 <script setup lang="ts">
 // import { RouterLink, RouterView } from 'vue-router'
 import { RouterView } from 'vue-router'
 </script>
-
-<template>
-  <RouterView />
-</template>
-
 <style>
-  * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
-body {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-    'Helvetica Neue', sans-serif;
-}
-
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
 </style>
